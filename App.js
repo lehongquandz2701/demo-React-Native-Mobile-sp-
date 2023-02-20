0255ec5488bc92291c5febea2557d135e43d5cfe
lehongquandz277 <lehongquandz2701@gmail.com>
2023-02-20 10:45:31 +0700

    demo

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { View, Text } from "react-native";
+import React from "react";
+import Welcome from "./Screens/Welcome";
+import FormLogin from "./Screens/FormLogin";
+import FormRegister from "./Screens/FromRegister";
+import FoodList from "./Screens/FoodList";
+import FoodItems from "./Screens/FoodList/FoodItems";
+import ProductGrid from "./Screens/ProductGrid";
+import Setting from "./Screens/setting";
+import UiTabs from "./Navigation/UiTabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Stack = createNativeStackNavigator();
+const screenOptionsMenu = (router) => {
+  return {};
+};
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name={"Welcome"} component={Welcome} />
+        <Stack.Screen name={"FormLogin"} component={FormLogin} />
+        <Stack.Screen name={"FormRegister"} component={FormRegister} />
+        <Stack.Screen name={"UiTabs"} component={UiTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
